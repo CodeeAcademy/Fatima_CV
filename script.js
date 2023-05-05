6859e54a428cdb2f8165841a1ef91b23ddec72c6
Code Academy <123144126+CodeeAcademy@users.noreply.github.com>
2023-05-05 13:55:05 +0300

    Upload The Main Design

---
+++ script.js
@@ -0,0 +1,15 @@
+function b1(){
+    document.getElementById("hd").innerHTML= "المعلومات الشخصية";
+    document.getElementById("det").innerHTML= "الاسم : فاطمة محمد مساعيد<br>رقم الهاتف : 0598101903<br>الجنسية : فلسطينية<br>مكان الاقامة : اريحا<br>تاريخ الميلاد : 4-10-2001";
+}
+function b2(){
+    document.getElementById("hd").innerHTML="اللغات";
+    document.getElementById("det").innerHTML = "العربية<br>الانجليزية<br>الفرنسية";
+}
+function b3(){
+    document.getElementById("hd").innerHTML="المؤهلات العلمية";
+    document.getElementById("det").innerHTML="توجيهي ادبي<br>بكالوريوس انظمة حاسوب<br>دورتين سايبر سكيوريتي<br>دورة قيادة حاسوب";
+}
+function b4(){
+    window.location="https://www.qou.edu/";
+}
